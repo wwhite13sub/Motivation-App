e1951cfd72e8ca25ca8ea0ec87cc56c90869ce8e
Wanda White <wanda.white32@yahoo.com>
2021-04-22 13:25:03 -0500

    adding modal markup

--- src/Components/Motivational/Motivational.jsx
+++ src/Components/Motivational/Motivational.jsx
@@ -49,11 +49,68 @@ class Motivational extends Component {
         );
     }
 
+    controlFormVisibility = () => {
+        if (!this.state.overlayFormShow) {
+            return;
+        }
+        return(
+           <>            
+            {/* Modal */}
+            <div  id="overlay-form" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div className="modal-dialog modal-lg">
+                <div className="modal-content">
+                    <form className="list">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="staticBackdropLabel"> Motivation day</h5>
+                        </div>
+                        <div className="modal-body">
+                        <div>
+                            <label htmlFor="add-date">Date: </label>
+                            <input 
+                                type="date" maxLength="10" placeholder="MM/DD/YYYY" 
+                                id="add-date" className="form-control"
+                                onChange
+                                value
+                                disabled
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="add-date">Motivation: </label>
+                            <div className="choice">
+                            
+                                <option>Select</option>
+                                                               
+                            </select>
+                            </div>
+                        </div>
+                        <div>                        
+                            <label htmlFor="journal-Entry">Journal Entry</label>
+                            <input
+                                maxLength ="50"  id="journal-Entry"
+                                type="task" className="form-control" placeholder="what is on your mind" 
+                                value=
+                            />
+                        </div>
+                  </div>
+                  
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick= >Cancel</button>
+                    <button type="submit" className="btn btn-success" onClick=</button>
+                  </div>
+                  {this.displayError()}
+                  </form>
+                </div>
+              </div>
+            </div>            
+            </>
+         );
+    }
+
     render() {
         return (
             <>            
                 <div className="container tables">        
-                    <button className="Adding" data-bs-toggle="modal" data-bs-target="#staticBackdrop" type="button" onClick={this.openAddForm}>Add Day</button>
+                    <button className="Adding" data-bs-toggle="modal" data-bs-target="#staticBackdrop" type="button" >Add Day</button>
                         <table className="table table-hover">
                             <thead>
                                 <tr className="headers">
@@ -65,7 +122,7 @@ class Motivational extends Component {
                                 </tr>
                             </thead>
                             <tbody>
-                                {this.getEntries()}
+                               
                             </tbody>
                         </table>
                     </div>
