30eebdf72f33086dd69ed7ca506eb91e35f9a670
Wanda White <wanda.white32@yahoo.com>
2021-04-22 13:12:29 -0500

    adding starter markup

--- src/Components/Motivational/Motivational.jsx
+++ src/Components/Motivational/Motivational.jsx
@@ -23,3 +23,30 @@ class Motivational extends Component {
             currentEntryIndex: 0
         };      
     }
+
+    render() {
+        return (
+            <>            
+                <div className="container tables">        
+                    <button className="Adding" data-bs-toggle="modal" data-bs-target="#staticBackdrop" type="button">Add Day</button>
+                        <table className="table table-hover">
+                            <thead>
+                                <tr className="headers">
+                                <th scope="col"> Date </th>
+                                <th scope="col"> Motivation</th>
+                                <th scope="col"> Journal</th>
+                                <th scope="col"> Edit</th>
+                                <th scope="col"> Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+               
+            </>
+        );
+    }
+}
+export default Motivational;
