6e423a1eb14b42f0bbbe5033d9bf49ff9e1ca919
Wanda White <wanda.white32@yahoo.com>
2021-04-23 23:07:33 -0500

    updating json for deploy

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ class App extends Component {
   render(){
     return (
      <>
-        <div className="">
           <NavBar />
           <Motivational />
-        </div>
       </>
     );
   }
